f74ef44d85ff48ef6dfdf30e9f19d47e9127fdb2
bipu990 <bipu0000000@gmail.com>
2026-07-24 04:55:27 +0600

    Solve Beecrowd 1002 - Area of a Circle
    This program calculates the area of a circle given its radius.

---
+++ 1002.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ int main ()
+ {
+     double x;
+     scanf ("%lf",&x);
+     
+     double pai=3.14159;
+     double m=pai*(x*x);
+     
+     printf ("A=%.4lf\n",m);
+     
+     return 0;
+ }
