b1ccbf029b7484521ec4ef4da85006a6e398b489
bipu990 <bipu0000000@gmail.com>
2026-07-26 04:40:38 +0600

    Solve Beecrowd 1036 - Bhaskara's Formula (UPDATE)

--- 1036.c
+++ 1036.c
@@ -7,17 +7,17 @@ int main ()
     scanf ("%lf %lf %lf",&a,&b,&c);
     
     double x = (b*b)-(4.0*a*c);
-    if (x>0) // ai (condition makeing)
+    if (x>=0 && a!=0)
     {
-        double m = pow(x,1.0/2);// ai (power)
-        double n = (-b+m)/(2.0*a);// ai (mistake-2, but should 2.0 cause-dauble)
+        double m = pow(x,1.0/2);
+        double n = (-b+m)/(2.0*a);
         printf ("R1 = %.5f\n",n);
         double o = (-b-m)/(2.0*a);
         printf ("R2 = %.5f\n",o);
     }
     else 
     {
-        printf ("Impossivel calcular");
+        printf ("Impossivel calcular\n");
     }
     
     return 0;
